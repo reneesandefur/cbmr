0e2ab5a33707bdd4b5b88aa957772738cefeac80
Renee Sandefur <reneersandefur@gmail.com>
2017-04-02 07:17:57 +0000

    updated homepage, added simple form, updated submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,4 +1,12 @@
 class SubmissionsController < ApplicationController
     def index
+        @submissions = Submission.all
+    end
+    
+    def new
+        @submission = Submission.new
+    end
+    
+    def create
     end
 end
